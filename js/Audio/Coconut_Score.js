8367325d14b24f7cc7f4114137491e23d7394812
vumg <ab17100222@gmail.com>
2020-01-29 14:37:49 -0600

    Changed directory

---
+++ js/Audio/Coconut_Score.js
@@ -0,0 +1,18 @@
+var sound = new Howl({
+  src: ["../../public/sounds/hollow_punch.wav"]
+});
+
+var coconutClicked = new Event("coconutClicked")
+
+export default function Punch() {
+  const punchListener = () => {
+    let coconut = document.getElementById("coconut");
+    coconut.onmouseover = () => (coconut.style.cursor = "pointer");
+    coconut.onclick = () => {
+      sound.play();
+      window.dispatchEvent(coconutClicked)
+    }
+  };
+
+  window.addEventListener("sunShowed", e => punchListener());
+}
