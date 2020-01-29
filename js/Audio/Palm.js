d6b83768809af34c81dea7d81d0d039ae3a8f354
vumg <ab17100222@gmail.com>
2020-01-29 14:38:41 -0600

    Palm Clicked Event

--- js/Audio/Palm.js
+++ js/Audio/Palm.js
@@ -2,12 +2,17 @@ var sound = new Howl({
   src: ["../../public/sounds/punch.wav"]
 });
 
+var palmClicked = new Event("palmClicked");
+
 export default function Punch() {
     
   const punchListener = () => {
     let palm = document.getElementById("palm2");
     palm.onmouseover = () => (palm.style.cursor = "pointer");
-    palm.onclick = () => sound.play();
+    palm.onclick = () => {
+        sound.play();
+        window.dispatchEvent(palmClicked)
+    }
   };
 
   window.addEventListener("sunShowed", e => punchListener());
