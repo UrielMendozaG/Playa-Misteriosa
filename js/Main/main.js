4cf37bb3ac3e8872d140986fe8530f269d3a596a
vumg <ab17100222@gmail.com>
2020-01-30 14:31:59 -0600

    Main

---
+++ js/Main/main.js
@@ -0,0 +1,39 @@
+import Introduction from "../Introduction/Introduction.js";
+import SceneShowing from "../Introduction/SceneShowing.js";
+import SunAnimation from "../Sun/SunShowing.js";
+
+import PalmBehavior from "../Game/Palm.js";
+import CoconutBehavior from "../Game/Single_Coconut.js";
+import ScoreBehavior from "../Game/Score.js";
+import CoconutScoreBehavior from "../Game/Coconut_Score.js"
+
+import ShowText from "../Game/Show_Text.js"
+import HobboBehavior from "../Game/Hobbo.js"
+
+import GithubBehavior from "../Game/Github.js"
+
+// 1.
+Introduction();
+
+SceneShowing();
+
+SunAnimation();
+
+// 2.
+
+PalmBehavior();
+
+CoconutBehavior();
+
+ScoreBehavior();
+
+CoconutScoreBehavior();
+
+// 3.
+
+ShowText()
+
+
+HobboBehavior()
+
+GithubBehavior()
