2d5a25e08ac33cb56a2779c0edcb71edaa455fd4
vumg <ab17100222@gmail.com>
2020-01-30 14:32:58 -0600

    Main Audio

---
+++ js/Audio/Audio.js
@@ -0,0 +1,28 @@
+import Ambience from "./Ambience.js";
+import Palm from "./Palm.js";
+
+import CoconutScore from "./Coconut_Score.js";
+import SingleCoconut from "./SingleCoconut.js";
+
+import Hobbo from "./Hobbo.js"
+import Text from "./Text.js"
+
+//cambiar el nombre del archivo a main audio
+
+// 1.
+
+Ambience();
+
+Palm();
+
+// 2.
+
+CoconutScore();
+
+SingleCoconut();
+
+// 3.
+
+Text();
+
+Hobbo();
