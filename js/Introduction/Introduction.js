be9ec3f23038943257b99fce12f9a2475a6e3b8e
vumg <ab17100222@gmail.com>
2020-01-30 14:32:24 -0600

    Introduction onload

--- js/Introduction/Introduction.js
+++ js/Introduction/Introduction.js
@@ -4,17 +4,29 @@ var introductionClicked = new Event("introductionClicked");
 
 export default function controller() {
   let section = document.getElementById("introduction");
+  let body = document.getElementById("body");
 
-  //efecto de desaparicion
-  window.onclick = () => {
-    anime({
-      targets: section,
-      opacity: 0,
-      easing: "easeInOutQuad",
-      complete: () => {
+  let clickToStart = document.getElementById("clickToStart");
+  let loading = document.getElementById("loading");
+
+
+  body.onload = () => {
+    clickToStart.style.display = "block"
+    clickToStart.style.textAlign = "center"
+
+    loading.style.display = "none"
+    
+    //efecto de desaparicion
+    window.onclick = () => {
+      anime({
+        targets: section,
+        opacity: 0,
+        easing: "easeInOutQuad",
+        complete: () => {
           section.style.display = "none";
-          window.dispatchEvent(introductionClicked)
-      }
-    });
+          window.dispatchEvent(introductionClicked);
+        }
+      });
+    };
   };
 }
