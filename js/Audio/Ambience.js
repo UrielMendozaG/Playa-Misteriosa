acdc18495678f131bbf316817aeca4915e365849
vumg <ab17100222@gmail.com>
2020-02-01 18:19:57 -0600

    Async fetch

--- js/Audio/Ambience.js
+++ js/Audio/Ambience.js
@@ -1,3 +1,14 @@
+async function getSound(){
+  try{
+    let response = await fetch("https://urielmendozag.github.io/Playa-Misteriosa/public/sounds/beach.mp3")
+    console.log(response.ok, response.status, response.statusText)
+  }catch(e){
+    console.log(e)
+  }
+}
+
+getSound()
+
 var sound = new Howl({
   src: ["https://urielmendozag.github.io/Playa-Misteriosa/public/sounds/beach.mp3"],
   volume: .2,
