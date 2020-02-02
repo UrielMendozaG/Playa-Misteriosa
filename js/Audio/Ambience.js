ed5fb83eff6ca616c40a6d8f17dbf67237ff3daf
vumg <ab17100222@gmail.com>
2020-02-01 18:37:14 -0600

    Fetch Data

--- js/Audio/Ambience.js
+++ js/Audio/Ambience.js
@@ -1,17 +1,25 @@
-async function getSound(){
-  try{
-    let response = await fetch("https://urielmendozag.github.io/Playa-Misteriosa/public/sounds/beach.mp3")
-    console.log(response.ok, response.status, response.statusText)
-  }catch(e){
-    console.log(e)
+let ambienceLoaded = new Event("ambienceLoaded");
+
+async function getSound() {
+  try {
+    let response = await fetch(
+      "https://urielmendozag.github.io/Playa-Misteriosa/public/sounds/beach.mp3"
+    );
+    if (response.ok) {
+      window.dispatchEvent(ambienceLoaded);
+    }
+  } catch (e) {
+    console.log(e);
   }
 }
 
-getSound()
+getSound();
 
 var sound = new Howl({
-  src: ["https://urielmendozag.github.io/Playa-Misteriosa/public/sounds/beach.mp3"],
-  volume: .2,
+  src: [
+    "https://urielmendozag.github.io/Playa-Misteriosa/public/sounds/beach.mp3"
+  ],
+  volume: 0.2
 });
 
 export default function Ambience() {
